e28563ef575096cb54b95a59d13a5c160469cba0
hedgehogus <research162@gmail.com>
2025-04-06 21:15:25 +0300

    Update metadata.ts

--- Typescript/metadata.ts
+++ Typescript/metadata.ts
@@ -20,3 +20,28 @@ console.log(height); */
 Reflect.defineMetadata('note', 'hi there', plane, 'color');
 const note = Reflect.getMetadata('note', plane, 'color');
 console.log(note);
+
+
+// -------------------------------------------
+@printMetadata
+class Plane {
+    color: string = 'red';
+
+    @markFunction('Hi there')
+    fly(): void {
+        console.log('vroom');
+    }
+}
+
+function markFunction(secret: string) {
+    return function (target: Plane, key: string) {
+        Reflect.defineMetadata('secret', secret, target, key);
+    }
+}
+
+function printMetadata(target: typeof Plane) {  //typeof Plane - reference to type of the constructor function
+    for ( let key in target.prototype) {
+        const metadata = Reflect.getMetadata('secret', target.prototype, key);
+        console.log(key, metadata);
+    }
+}
